3a04512b02295a150bead6c84f09ee7f5541a50f
student-a502 <student-a502@PC-A502-21>
2024-12-19 12:49:29 +0500

    Программирование окна recoveryю.Липина

--- RegIN_Lipina/Pages/Recovery.xaml.cs
+++ RegIN_Lipina/Pages/Recovery.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +12,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +24,116 @@ namespace RegIN_Lipina.Pages
     /// </summary>
     public partial class Recovery : Page
     {
+        string OldLogin;
+        bool IsCapture = false;
+        private void CorrectLogin()
+        {
+            if (OldLogin != TbLogin.Text)
+            {
+                SetNotification("Hi, " + MainWindow.mainWindow.UserLogIn.Name, Brushes.Black);
+                try
+                {
+
+                    BitmapImage biImg = new BitmapImage();
+
+                    MemoryStream ms = new MemoryStream(MainWindow.mainWindow.UserLogIn.Image);
+
+                    biImg.BeginInit();
+                    biImg.EndInit();
+                    ImageSource imgSrc = biImg;
+                    DoubleAnimation StartAnimation = new DoubleAnimation();
+                    StartAnimation.From = 1;
+                    StartAnimation.To = 0;
+                    StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+
+                    StartAnimation.Completed += delegate
+                    {
+                        IUser.Source = imgSrc;
+                        DoubleAnimation EndAnimation = new DoubleAnimation();
+                        EndAnimation.From = 0;
+                        EndAnimation.To = 1;
+                        EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                        IUser.BeginAnimation(Image.OpacityProperty, EndAnimation);
+                    };
+                    IUser.BeginAnimation(Image.OpacityProperty, StartAnimation);
+                }
+                catch (Exception exp)
+                {
+                    Debug.WriteLine(exp.Message);
+                };
+                OldLogin = TbLogin.Text;
+                SendNewPassword();
+            }
+        }
+        private void InCorrectLogin()
+        {
+            if (LNameUser.Content != "")
+            {
+                LNameUser.Content = "";
+                DoubleAnimation StartAnimation = new DoubleAnimation();
+                StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+                StartAnimation.Completed += delegate
+                {
+                    DoubleAnimation EndAnimation = new DoubleAnimation();
+                    EndAnimation.From = 0;
+                    EndAnimation.To = 1;
+                    EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                    IUser.BeginAnimation(OpacityProperty, EndAnimation);
+                };
+                IUser.BeginAnimation(OpacityProperty, StartAnimation);
+            }
+            if (TbLogin.Text.Length > 0)
+                SetNotification("Login is incorrect", Brushes.Red);
+        }
+        private void CorrectCapture()
+        {
+            Capture.IsEnabled = false;
+            IsCapture = true;
+            SendNewPassword();
+        }
+
         public Recovery()
         {
             InitializeComponent();
+
+            MainWindow.mainWindow.UserLogIn.HandlerCorrectLogin += CorrectLogin;
+            MainWindow.mainWindow.UserLogIn.HandlerInCorrectLogin += InCorrectLogin;
+            Capture.HandlerCorrectCapture += CorrectCapture;
+        }
+        private void SetLogin(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                MainWindow.mainWindow.UserLogIn.GetUserLogin(TbLogin.Text);
+        }
+        private void SetLogin(object sender, RoutedEventArgs e) =>
+        MainWindow.mainWindow.UserLogIn.GetUserLogin(TbLogin.Text);
+        public void SendNewPassword()
+        {
+            if
+            (IsCapture)
+                if (MainWindow.mainWindow.UserLogIn.Password != String.Empty)
+                {
+                    DoubleAnimation StartAnimation = new DoubleAnimation();
+                    StartAnimation.From = 1;
+                    StartAnimation.To = 0;
+                    StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+                    StartAnimation.Completed += delegate
+                    {
+                        IUser.Source = new BitmapImage(new Uri("pack://application:,,,/Images/ic-mail.png"));
+                        DoubleAnimation EndAnimation = new DoubleAnimation();
+                        EndAnimation.From = 0;
+                        EndAnimation.To = 1;
+                        EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                        IUser.BeginAnimation(OpacityProperty, EndAnimation);
+                    };
+                    IUser.BeginAnimation(OpacityProperty, StartAnimation);
+                    SetNotification("An email has been sent to your email.", Brushes.Black);
+                    MainWindow.mainWindow.UserLogIn.CrateNewPassword();
+                }
+        }
+        private void OpenLogin(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow.mainWindow.OpenPage(new Login());
         }
     }
 }
