bf1958a5652a9ce483283d3752abcb320e4312fd
student-a502 <student-a502@PC-A502-21>
2024-12-19 10:55:02 +0500

    Разработанный класс отправки сообщения на почту.Lipina

---
+++ RegIN_Lipina/Classes/SendMail.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace RegIN_Lipina.Classes
+{
+    public class SendMail
+    {
+
+        public static void SendMessage(string Message, string To)
+        {
+
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+
+                Port = 587,
+
+                Credentials = new NetworkCredential("yandex@yandex.ru", "password"),
+
+                EnableSsl = true,
+            };
+
+            smtpClient.Send("landaxer@yandex.ru", To, "Проект RegIn", Message);
+        }
+    }
+}
