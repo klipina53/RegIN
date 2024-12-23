7b188ed82896dcba16eb420e606ab6a40a5e8896
student-a502 <student-a502@PC-A502-21>
2024-12-23 10:28:37 +0500

    Программирование окна Confirmation. Lipina

--- RegIN_Lipina/Pages/Coniformation.xaml.cs
+++ RegIN_Lipina/Pages/Coniformation.xaml.cs
@@ -20,9 +20,86 @@ namespace RegIN_Lipina.Pages
     /// </summary>
     public partial class Coniformation : Page
     {
-        public Coniformation()
+        public enum TypeConfirmation
+        {
+            Login, Regin
+        }
+        TypeConfirmation ThisTypeConfirmation;
+
+        public int Code = 0;
+
+        public Confirmation(TypeConfirmation TypeConfirmation)
         {
             InitializeComponent();
+
+            ThisTypeConfirmation = TypeConfirmation;
+
+            SendMailCode();
         }
+        public void SendMailCode()
+        {
+
+            Code = new Random().Next(100000, 999999);
+
+            Classes.SendMail.SendMessage($"Login code: {Code}", MainWindow.mainWindow.userLogin.Login);
+
+            Thread TSendMailCode = new Thread(TimerSendMailCode);
+
+            TSendMailCode.Start();
+
+        }
+        public void TimerSendMailCode()
+        {
+            for (int i = 0; i < 60; i++)
+            {
+
+                Dispatcher.Invoke(() => LTimer.Content = $"A second message can be sent after {60 - i} seconds");
+
+                Thread.Sleep(1000);
+            }
+
+            Dispatcher.Invoke(() =>
+            {
+
+                BSendMessage.IsEnabled = true;
+
+                LTimer.Content = "";
+
+            });
+        }
+        private void SendMail(object sender, RoutedEventArgs e) => SendMailCode();
+
+        private void SetCode(object sender, KeyEventArgs e)
+        {
+
+            if (TbCode.Text.Length == 6) SetCode();
+
+        }
+
+        private void SetCode(object sender, RoutedEventArgs e) => SetCode();
+
+        void SetCode()
+        {
+
+            if (TbCode.Text == Code.ToString() && TbCode.IsEnabled == true)
+            {
+
+                TbCode.IsEnabled = false;
+
+                if (ThisTypeConfirmation == TypeConfirmation.Login) MessageBox.Show("Авторизация пользователя успешно подтверждена.");
+
+                else
+                {
+
+                    MainWindow.mainWindow.userLogin.SetUser();
+
+                    MessageBox.Show("Регистрация пользователя успешно подтверждена.");
+
+                }
+            }
+        }
+
+        private void OpenLogin(object sender, MouseButtonEventArgs e) => MainWindow.mainWindow.OpenPage(new Login());
+
     }
 }
